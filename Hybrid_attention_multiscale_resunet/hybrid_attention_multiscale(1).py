c71cd9e9fe09f33bbe1f155dc3a781d1143a9b52
Aryan Pandit <aryanpandit.1205@gmail.com>
2025-05-30 19:38:44 +0530

    changes

--- Hybrid_attention_multiscale_resunet/hybrid_attention_multiscale(1).py
+++ Hybrid_attention_multiscale_resunet/hybrid_attention_multiscale(1).py
@@ -10,7 +10,7 @@ Original file is located at
 from google.colab import drive
 drive.mount('/content/drive')
 
-!pip install scikit-image
+# pip install scikit-image
 
 import numpy as np
 import matplotlib.pyplot as plt
